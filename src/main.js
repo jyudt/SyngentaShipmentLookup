4b38bf0adfe35cc7f5a71aeb2d3ff53883da8449
System Administrator <root@USNYMJYUDT01.infor.com>
2019-06-24 14:04:04 -0400

    removed resources folder and default shipment ID

--- src/main.js
+++ src/main.js
@@ -101,7 +101,7 @@ export default class HomeScreen extends React.Component {
         <Text>{`\n`}</Text>
         <Image style={{width:230, height:70, justifyContent: 'flex-end'}} source={require('./images/Syngenta_Logo.png')}/>
         <View style={styles.container}>
-          <TextInput style={styles.instructions} defaultValue = 'CNC0000175' placeholder="Shipment ID" onChangeText={(shipID) => this.setState({shipID})}/><Button onPress={() => {this.lookup("ShipID")}} title="Search"/>
+          <TextInput style={styles.instructions} placeholder="Shipment ID" onChangeText={(shipID) => this.setState({shipID})}/><Button onPress={() => {this.lookup("ShipID")}} title="Search"/>
           <Text>{`\n\n\n`}</Text>
           <TextInput style={styles.instructions} placeholder="PO Number" onChangeText={(PONum) => this.setState({PONum})}/><Button onPress={() => {this.lookup("PONum")}} title="Search"/>
           <Text>{`\n\n\n\n\n\n`}</Text>
