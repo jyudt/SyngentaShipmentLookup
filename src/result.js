ec6b3ff3f70a5b0b5a0e776d6dfbcb4075287906
System Administrator <root@USNYMJYUDT01.infor.com>
2019-06-26 16:08:04 -0400

    changed result formatting

--- src/result.js
+++ src/result.js
@@ -19,6 +19,8 @@ export default class ResultScreen extends React.Component {
     this.state.attrib = this.props.navigation.getParam('attrib','ERROR');
     this.state.num = this.props.navigation.getParam('num','ERROR');
     this.state.delDate = new Date(this.state.obj.Container[0].LineItems[0].RequiredDeliveryDate.value);
+    this.state.shipID = this.state.obj.ShipmentID;
+    this.state.PONum = this.state.obj.Container[0].LineItems[0].PONumber;
     this.state.ident = (this.state.attrib == 'ShipID') ? "ID " + this.state.num : "PO Number " + this.state.num;
   };
   static navigationOptions = {
@@ -31,7 +33,8 @@ export default class ResultScreen extends React.Component {
         <Text>{`\n`}</Text>
         <Image style={{width:230, height:70, justifyContent: 'flex-start'}} resizeMethod={'resize'} source={require('./images/Syngenta_Logo.png')}/>
           <View style={styles.container}>
-            <Text style = {styles.welcome}>{'Your shipment with '}{this.state.ident}{' will arrive by: \n\n'}{this.state.delDate.toString()}{'.'}{`\n\n\n\n\n\n`}</Text>
+            <Text style = {styles.welcome}><Text style={{fontWeight:'bold'}}>{'Your shipment with: \n'}</Text>{'Shipment ID: '}{this.state.shipID}{'\n'}{'PO Number: '}{this.state.PONum}{'\n\n'}<Text style={{fontWeight:'bold'}}>{'Is scheduled to arrive on: \n'}</Text>
+            {this.state.delDate.toDateString()}{' | '}{this.state.delDate.getHours()}{':'}{(this.state.delDate.getMinutes() == 0 ? '00' : this.state.delDate.getMinutes())}{'\n\n\n\n\n\n'}</Text>
           </View>
       </View>
     );
